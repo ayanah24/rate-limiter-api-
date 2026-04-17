a16f14f0f308c43e5fda611735df40485a30c764
ayanah24 <ayan27452@gmail.com>
2026-04-17 18:19:01 +0530

    feat: implement rate limiter middleware and integrate into application server

--- app.js
+++ app.js
@@ -21,7 +21,6 @@ app.get('/health', (req, res) => {
     });
 });
 
-
 app.listen(process.env.PORT, () => {
     console.log(`Server is running on port ${process.env.PORT}`);
     connectRedis();
--- src/middleware/rateLimiter.js
+++ src/middleware/rateLimiter.js
@@ -1,7 +1,6 @@
 import fixedWindowLimiter from '../limiters/fixedWindow.js';
 
 const rateLimiterMiddleware = async (req, res, next) => {
-    console.log("🔥 MIDDLEWARE HIT:", req.url);
     try {
         const clientIP = req.ip;
         const { allowed, cnt, limit, remaining, retryAfter } = await fixedWindowLimiter(clientIP);
@@ -10,7 +9,6 @@ const rateLimiterMiddleware = async (req, res, next) => {
         res.set('x-RateLimit-Remaining', remaining);
         res.set('x-RateLimit-Count', cnt);
         res.set('X-RateLimit-Reset', retryAfter);
-        console.log("retryAfter:", retryAfter);
         if (!allowed) {
             return res.status(429).json({
                 error: 'Too Many Requests',
